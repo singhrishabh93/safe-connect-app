69e501dec02a1cdb1cb0ca8f04c1ca31981214d1
Rishabh Singh <singhrishabh1670@gmail.com>
2024-03-01 13:38:43 +0530

    added functionality to check for active internet connection

--- lib/screens/SplashScreen/splashScreen.dart
+++ lib/screens/SplashScreen/splashScreen.dart
@@ -1,64 +1,127 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:safe_connect/bottomNavigationBar.dart';
 import 'package:safe_connect/theme.dart';
 
 class splashScreen extends StatefulWidget {
-  const splashScreen({super.key});
+  const splashScreen({Key? key}) : super(key: key);
 
   @override
   State<splashScreen> createState() => _splashScreenState();
 }
 
 class _splashScreenState extends State<splashScreen> {
+  bool _hasInternet = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkInternet();
+  }
+
+  Future<void> _checkInternet() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      setState(() {
+        _hasInternet = true;
+      });
+      _redirectAfterDelay();
+    } else {
+      // No internet connection
+      _showNoInternetPopup();
+    }
+  }
+
+  Future<void> _redirectAfterDelay() async {
+    await Future.delayed(Duration(seconds: 3));
+    // Navigate to home screen
+    Get.to(() => bottomNavigationBar());
+  }
+
+  void _showNoInternetPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          title: Text("No Internet Connection"),
+          content: Text("Please check your internet connection and try again."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                _checkInternet();
+              },
+              child: Text("Try Again"),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          width: deviceWidth,
-          height: deviceHeight,
-          decoration: const BoxDecoration(
-            color: Color.fromRGBO(255, 255, 255, 1),
-          ),
-          child: Stack(children: <Widget>[
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Stack(
+          children: <Widget>[
             Positioned(
-                top: MediaQuery.of(context).size.height - 200,
-                left: MediaQuery.of(context).size.width / 2 - 42,
-                child: Container(
-                    width: 84,
-                    height: 117,
-                    child: const Positioned(
-                        top: 87,
-                        left: 0,
-                        child: Text(
-                          'MADE WITH FLUTTER ❤️',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Color.fromRGBO(0, 0, 0, 1),
-                              fontFamily: 'Inter',
-                              fontSize: 12,
-                              letterSpacing: 0,
-                              fontWeight: FontWeight.normal,
-                              height: 1),
-                        )))),
-            Positioned(
-                child: Center(
+              top: MediaQuery.of(context).size.height - 200,
+              left: MediaQuery.of(context).size.width / 2 - 42,
               child: Container(
+                width: 84,
+                height: 117,
+                child: const Positioned(
+                  top: 87,
+                  left: 0,
+                  child: Text(
+                    'MADE WITH FLUTTER ❤️',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Inter',
+                      fontSize: 12,
+                      letterSpacing: 0,
+                      fontWeight: FontWeight.normal,
+                      height: 1,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              child: Center(
+                child: Container(
                   width: 200,
                   height: 100,
-                  decoration: const BoxDecoration(
+                  decoration: BoxDecoration(
                     image: DecorationImage(
-                        image: AssetImage('assets/images/logo.png'),
-                        fit: BoxFit.fitWidth),
-                  )),
-            )),
-            // Positioned(
-            //     bottom: 100,
-            //     child: Container(
-            //       margin: EdgeInsets.all(130),
-            //       child: Text(
-            //         "Made with Flutter ❤️",
-            //       ),
-            //     )),
-          ])),
+                      image: AssetImage('assets/images/logo.png'),
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
