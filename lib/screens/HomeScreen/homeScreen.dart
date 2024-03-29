e16ce910691f5149195f665c11995c5ad41fcfa0
Rishabh Singh <singhrishabh1670@gmail.com>
2024-03-29 22:17:45 +0530

    HomeScreen UI updated

--- lib/screens/HomeScreen/homeScreen.dart
+++ lib/screens/HomeScreen/homeScreen.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:safe_connect/screens/HomeScreen/HomeScreenElements/carousel.dart';
+import 'package:safe_connect/screens/HomeScreen/HomeScreenElements/fourthChild.dart';
 import 'package:safe_connect/screens/HomeScreen/HomeScreenElements/newSecondChild.dart';
 import 'package:safe_connect/screens/HomeScreen/HomeScreenElements/newThirdChild.dart';
-import 'package:safe_connect/screens/HomeScreen/HomeScreenElements/secondChild.dart';
-import 'package:safe_connect/screens/HomeScreen/HomeScreenElements/servicesContainer.dart';
 import 'package:safe_connect/screens/HomeScreen/HomeScreenElements/welcomeChild.dart';
-import 'package:safe_connect/sideNavigationBar.dart';
-import 'package:safe_connect/theme.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -44,8 +40,11 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(height: 10),
               Container(
                   width: MediaQuery.of(context).size.width - 50,
+                  child: fourthChild()),
+              Container(
+                  width: MediaQuery.of(context).size.width - 50,
                   child: newThirdChild()),
-              SizedBox(height: 100),
+              SizedBox(height: 10),
             ],
           ),
         ),
