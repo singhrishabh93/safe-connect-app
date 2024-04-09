a2a593639f35f56de4a5527c33bb330a271c3303
Rishabh Singh <singhrishabh1670@gmail.com>
2024-04-09 22:15:34 +0530

    Device Wallpaper Registration form added

---
+++ lib/screens/QrRegistrationScreen/qrWallpaper.dart
@@ -0,0 +1,429 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:safe_connect/screens/HomeScreen/homeScreen.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class QRWallpaper extends StatefulWidget {
+  const QRWallpaper({Key? key}) : super(key: key);
+
+  @override
+  _QRWallpaperState createState() => _QRWallpaperState();
+}
+
+class _QRWallpaperState extends State<QRWallpaper> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _deviceNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _contactNoController = TextEditingController();
+  final TextEditingController _emergencyContactNoController =
+      TextEditingController();
+  String _qrData = '';
+  bool _showQRData = false;
+  String? _selectedDeviceType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          },
+          icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+        ),
+        title: Text(
+          'Registration',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        color: Colors.black,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Device Registration',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color(0xffFFB13D),
+                    fontFamily: 'Gilroy',
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                Text(
+                  'Fill out this registration form & generate the QR',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontFamily: 'Gilroy',
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  style: TextStyle(color: Colors.white),
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    _nameController.value = _nameController.value.copyWith(
+                      text: value.replaceAll(RegExp(r'[^a-zA-Z\s]'), ''),
+                      selection: TextSelection.collapsed(offset: value.length),
+                      composing: TextRange.empty,
+                    );
+                  },
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  child: DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelText: 'Device Type',
+                      labelStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                    ),
+                    value: _selectedDeviceType,
+                    items:
+                        ['Smartphone', 'Tablet', 'Laptop'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                    style: TextStyle(color: Colors.white),
+                    dropdownColor: Colors.black,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _selectedDeviceType = value;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  style: TextStyle(color: Colors.white),
+                  controller: _deviceNameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Device Name',
+                    labelStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  onChanged: (value) {
+                    _deviceNameController.value =
+                        _deviceNameController.value.copyWith(
+                      text: value.replaceAll(RegExp(r'[^a-zA-Z0-9\s]'), ''),
+                      selection: TextSelection.collapsed(offset: value.length),
+                      composing: TextRange.empty,
+                    );
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  style: TextStyle(color: Colors.white),
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    if (!_isValidEmailFormat(value)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  style: TextStyle(color: Colors.white),
+                  controller: _contactNoController,
+                  decoration: const InputDecoration(
+                    labelText: 'Contact No.',
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.phone,
+                  maxLength: 10,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your contact number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  style: TextStyle(color: Colors.white),
+                  controller: _emergencyContactNoController,
+                  decoration: const InputDecoration(
+                    labelText: 'Emergency Contact No.',
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.phone,
+                  maxLength: 10,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your contact number';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 40.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    backgroundColor: Color(0xffFFB13D),
+                  ),
+                  onPressed: _onGenerateQRPressed,
+                  child: const Text(
+                    'Generate QR Code',
+                    style: TextStyle(color: Colors.black, fontSize: 14),
+                  ),
+                ),
+                if (_showQRData)
+                  Column(
+                    children: [
+                      const SizedBox(height: 20.0),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: Container(
+                                padding: const EdgeInsets.all(0.0),
+                                child: QrImageView(
+                                  data: _qrData,
+                                  version: QrVersions.auto,
+                                  size: 120.0,
+                                  backgroundColor: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 20.0),
+                              child: Text(
+                                'Stay Connected to loved ones Download the QR for ${_deviceNameController.text}',
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                  fontFamily: 'Gilroy',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _onGenerateQRPressed() async {
+    if (_formKey.currentState!.validate()) {
+      await _saveDataToFirestore(); // Save data to Firestore
+      _generateQRFromFirestoreData(); // Generate QR code from Firestore data
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Form Error"),
+            content: const Text("Please fill the form correctly."),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("OK"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  Future<void> _saveDataToFirestore() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      final mobileNumber = user!.phoneNumber;
+      final deviceName = _deviceNameController.text;
+
+      // Save registration data to Firestore under 'registrationData' collection
+      await FirebaseFirestore.instance
+          .collection('registeredDevices')
+          .doc(mobileNumber)
+          .set({
+        'name': _nameController.text,
+        'deviceType': _selectedDeviceType,
+        'deviceName': deviceName,
+        'email': _emailController.text,
+        'contactNo': _contactNoController.text,
+        'emergencyContactNo': _emergencyContactNoController.text,
+      });
+
+      // Save data to 'wallpaperQr' collection
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(mobileNumber)
+          .collection('wallpaperQr')
+          .doc(deviceName)
+          .set({
+        'name': _nameController.text,
+        'deviceType': _selectedDeviceType,
+        'email': _emailController.text,
+        'contactNo': _contactNoController.text,
+        'emergencyContactNo': _emergencyContactNoController.text,
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _generateQRFromFirestoreData() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      final mobileNumber = user!.phoneNumber;
+
+      // Retrieve data from Firestore
+      final snapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(mobileNumber)
+          .collection('wallpaperQr')
+          .doc(_deviceNameController.text) // Use device name as document ID
+          .get();
+
+      if (snapshot.exists) {
+        final data = snapshot.data() as Map<String, dynamic>;
+
+        // Generate QR data
+        final qrData =
+            'Name: ${data['name']}, Device Type: ${data['deviceType']}, Device Name: ${data['deviceName']}, Email: ${data['email']}, Contact No.: ${data['contactNo']}, Emergency Contact No.: ${data['emergencyContactNo']}';
+
+        setState(() {
+          _qrData = qrData;
+          _showQRData = true;
+        });
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _deviceNameController.dispose();
+    _emailController.dispose();
+    _contactNoController.dispose();
+    _emergencyContactNoController.dispose();
+    super.dispose();
+  }
+
+  bool _isValidEmailFormat(String email) {
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
+}
