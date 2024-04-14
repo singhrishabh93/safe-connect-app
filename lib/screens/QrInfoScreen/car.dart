349669e00639c74bdc8dae37c8f0e1b72a05488b
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-04-14 21:04:10 +0530

    Completed the UI of Car info page

--- lib/screens/QrInfoScreen/car.dart
+++ lib/screens/QrInfoScreen/car.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:safe_connect/bottomNavBar.dart';
 import 'package:safe_connect/screens/QrRegistrationScreen/qrCar.dart';
 import 'package:lottie/lottie.dart';
 
@@ -11,63 +13,91 @@ class carQr extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Container(
-          child: Image.asset("assets/images/SafeConnect 1.png"),
-        ),
-        backgroundColor: Colors.grey.shade100,
-        elevation: 0,
-        scrolledUnderElevation: 0,
-        centerTitle: true,
-      ),
+          backgroundColor: Colors.black,
+          elevation: 0,
+          scrolledUnderElevation: 0,
+          centerTitle: true,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => bottomNavigationBar()));
+              },
+              icon: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.white,
+              ))),
       body: SingleChildScrollView(
         child: Container(
-          color: Colors.white,
           child: Column(
             children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 40,
-                child: const Center(
-                    child: Text(
-                  "Product",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-              ),
-              const SizedBox(
-                height: 10,
+              Center(
+                child: Container(
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Vehicle',
+                          style: TextStyle(
+                              fontFamily: 'Gilroy',
+                              fontSize: 20.0,
+                              color: Colors.white),
+                        ),
+                        TextSpan(
+                          text: ' Registration',
+                          style: TextStyle(
+                            fontFamily: 'Gilroy',
+                            fontSize: 20.0,
+                            color: Color(0xFFFFB13D),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
               SizedBox(
                 width: MediaQuery.of(context).size.width,
                 height: 400,
                 child: Lottie.asset('assets/images/carLottie.json'),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Jeevan Raksha | Car",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Description",
-                style: TextStyle(fontSize: 16, color: Colors.grey),
-              ),
-              // const SizedBox(
-              //   height: 10,
-              // ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Center(
-                  child: RichText(
-                      text: const TextSpan(
-                          text:
-                              "Fill up all the details and generate your unique QR code, paste the QR code stickers on the front and rear windscreen of the car while round stickers are pasted on the and other relevant places of the car. Unfortunately, in the case of a car accident, any person present at the accident site or the police can scan the sticker and immediately give information about the accident to your family and friends along with photos/videos, so that you can get proper medical facility and other help on time. Getting the best help can save your life.",
-                          style: TextStyle(color: Colors.black))),
+              Center(
+                child: Container(
+                  child: Text(
+                    'How to use ?',
+                    style: TextStyle(
+                        fontFamily: 'Gilroy',
+                        fontSize: 20.0,
+                        color: Colors.white),
+                  ),
                 ),
-              )
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                  ),
+                  child: Container(
+                    child: RichText(
+                      text: TextSpan(
+                        text:
+                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                        style: TextStyle(
+                          fontFamily: 'Gilroy',
+                          fontSize: 15.0,
+                          color: Colors.white38,
+                        ),
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -78,15 +108,16 @@ class carQr extends StatelessWidget {
           onPressed: () {
             Get.to(() => const QRGenerator());
           },
-          label: const Text(
+          label: Text(
             "Register Your Car",
-            style: TextStyle(fontSize: 20, color: Colors.white),
+            style: TextStyle(fontSize: 20, color: Colors.black),
           ),
           isExtended: true,
-          backgroundColor: const Color(0xFF3199E4),
+          backgroundColor: const Color(0xFFFFB13D),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      backgroundColor: Colors.black,
     );
   }
 }
